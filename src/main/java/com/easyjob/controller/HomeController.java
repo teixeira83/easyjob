1a527680694a960412dd5e6a0072ac5069ab8ef3
z3r0 <z3r0@z3r0>
2019-06-12 20:08:22 -0300

    Implementação da HomePage

--- src/main/java/com/easyjob/controller/HomeController.java
+++ src/main/java/com/easyjob/controller/HomeController.java
@@ -3,11 +3,17 @@ package com.easyjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 public class HomeController {
-	
+		
 	@GetMapping("/")
 	public String home() {
 		return "Home";
+	}
+	
+	@GetMapping("/aboutus")
+	public String about() {
+		return "About";
 	}
 }
